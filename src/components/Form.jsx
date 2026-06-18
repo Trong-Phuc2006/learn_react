5f6d0506192ab9cb35c9647a5da6f63a7d00087c
TrongPhuc-2006 <ltrongphuc06@gmail.com>
2026-06-19 00:03:25 +0700

    Input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,9 +2,19 @@ import { useState } from "react";
 
 export default function Form() {
   const [name, setName] = useState("");
+  const [kaba, setKaba] = useState("");
+  const [submission, setSubmission] = useState("");
 
   function handleChange(e) {
     setName(e.target.value);
+  }
+  function handleKaba(e)
+  {
+    setKaba(e.target.value);
+  }
+  function submit()
+  {
+    setSubmission(name+kaba);
   }
 
   return (
@@ -14,12 +24,14 @@ export default function Form() {
       <form>
         <input
           onChange={handleChange}
+          size = "1"
           type="text"
           value={name}
         />
+        <input onChange={handleKaba} type="text" value={kaba}></input>
+        <input type="submit" onClick={submit}></input>
       </form>
-
-      <p>Name: {name}</p>
+      <p>After submission = {submission}</p>
     </div>
   );
 }
